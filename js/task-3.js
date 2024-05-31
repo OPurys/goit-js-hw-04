bfb26e86f02a5d87c5667f5de41c2bf1866af870
Oleh Purys <wenger1901@gmail.com>
2024-05-31 14:43:09 +0300

    push

--- js/task-3.js
+++ js/task-3.js
@@ -1,19 +1,24 @@
-'use strict';
+const profile = {
+  username: 'Jacob',
+  playTime: 300,
 
-function filterArray(numbers, value) {
-  const arrayOfNumbers = [];
+  changeUsername(newName) {
+    this.username = newName;
+  },
 
-  for (const element of numbers) {
-    if (element > value) {
-      arrayOfNumbers.push(element);
-    }
-  }
+  updatePlayTime(hours) {
+    this.playTime += hours;
+  },
 
-  return arrayOfNumbers;
-}
+  getInfo() {
+    return `${this.username} has ${this.playTime} active hours!`;
+  },
+};
 
-console.log(filterArray([1, 2, 3, 4, 5], 3)); // [4, 5]
-console.log(filterArray([1, 2, 3, 4, 5], 4)); // [5]
-console.log(filterArray([1, 2, 3, 4, 5], 5)); // []
-console.log(filterArray([12, 24, 8, 41, 76], 38)); // [41, 76]
-console.log(filterArray([12, 24, 8, 41, 76], 20)); // [24, 41, 76]
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername('Marco');
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
